e15e38a688d2a69f240faadb44fcef6941c5dfe3
olumide <o3cloudng@gmail.com>
2024-06-19 13:03:44 +0100

    Dashboard links added

--- core/settings.py
+++ core/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'core.wsgi.application'
 DATABASES = {
         'default': dj_database_url.config(
             default=config('DB_URL'),
-            conn_max_age=600,
+            conn_max_age=60,
             conn_health_checks=True,
         )
     }
